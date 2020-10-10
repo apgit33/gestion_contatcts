e7e91c83b6251a00d0bba5ccf0b761d5c2b5a487
apgit33 <adrienpaturot@yahoo.fr>
2020-10-10 19:19:57 +0200

    Update template

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -10,8 +10,15 @@
         <ul class="list-group list-group-flush">
             <li class="list-group-item">Email : {{ $user->email }}</li>
             <li class="list-group-item">Admin : @php echo ($user->is_admin) ? 'Yes':'No'@endphp</li>
+                <div class="card-header">
+                    Adresse : 
+                </div>
+                @forelse ($adresses as $adresse)
+                    <li class="list-group-item">{{ $adresse->adresse }}</li>
+                @empty
+                    <li class="list-group-item">Pas d'adresse</li>
+                @endforelse
         </ul>
     </div>
 </div>
-
 @endsection
